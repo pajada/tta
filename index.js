b23165fe53e5f4b6580ae89d1e9b94b4cc4c460f
Dushko Jordanovski <jordanovskid@gmail.com>
2021-11-01 00:03:40 +0000

    Calculating scores

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function parseBatch(elem, $) {
             var match = (_a = tds
                 .eq(1)
                 .html()) === null || _a === void 0 ? void 0 : _a.match(/session-code(&quot;|")>(\w+)<\/span>/);
-            var replayCode = match && match[2];
+            var replayCode = match ? match[2] : "";
             var scores = [];
             $("div", tds.eq(2)).each(function (_ix, el) {
                 scores.push($(el)
@@ -62,4 +62,63 @@ function extractRaw(file) {
     }
     return parsed;
 }
-console.log(extractRaw("test-data/international-season-14.html").length);
+var scoring = {
+    3: { 1: 5, 2: 2, 3: 0 },
+    4: { 1: 6, 2: 3, 3: 1, 4: 0 },
+};
+function processRaw(parsed) {
+    var result = {};
+    var _loop_1 = function (item) {
+        var _a = item.name.match(/\s(\w+)\s(\d+)\sgame\s(\d+)/), _ = _a[0], le = _a[1], gr = _a[2], ga = _a[3];
+        var league = le.toLowerCase();
+        var groupNumber = parseInt(gr, 10);
+        var gameNumber = parseInt(ga, 10);
+        result[league] = result[league] || {};
+        var group = (result[league][groupNumber] = result[league][groupNumber] || {
+            number: groupNumber,
+            games: [],
+            standings: [],
+        });
+        var game = {
+            name: "Game " + gameNumber,
+            code: item.replayCode,
+            date: item.date,
+            scores: item.scores.map(function (score) {
+                var player = score.length === 2 ? score[0] : score[1];
+                var points = score.length === 2 ? score[1] : parseInt(score[2], 10);
+                return {
+                    player: player,
+                    score: points === "TIMED_OUT"
+                        ? "TIMED OUT"
+                        : points,
+                };
+            }),
+        };
+        game.scores.forEach(function (score, index) {
+            var standing = group.standings.find(function (s) { return s.player === score.player; });
+            if (!standing) {
+                group.standings.push((standing = { player: score.player, points: 0 }));
+            }
+            if (score.score === "TIMED OUT") {
+                // No points for timing out
+                return;
+            }
+            // Find all players that have the same score, resignations included
+            // Take their ranking and then assign and divide points
+            var shared = game.scores
+                .map(function (sc, ix) { return (sc.score === score.score ? ix + 1 : 0); })
+                .filter(function (rank) { return rank; });
+            standing.points +=
+                shared.reduce(function (acc, rank) { return acc + scoring[game.scores.length][rank]; }, 0) / shared.length;
+        });
+        group.games.push(game);
+    };
+    for (var _i = 0, parsed_1 = parsed; _i < parsed_1.length; _i++) {
+        var item = parsed_1[_i];
+        _loop_1(item);
+    }
+    return result;
+}
+var r = processRaw(extractRaw("test-data/international-season-14.html"));
+console.log(JSON.stringify(r, null, 2));
+// processRaw(extractRaw("test-data/intermezzo-season-11.html").slice(4, 5));
--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ import path from "path";
 type RawParsed = {
   name: string;
   replayCode: string;
-  results: string[][];
+  scores: string[][];
   date: string;
 };
 
@@ -76,38 +76,107 @@ function extractRaw(file: string) {
   return parsed;
 }
 
-type League = {
-  division: number;
+type League =
+  | "wood"
+  | "bronze"
+  | "silver"
+  | "gold"
+  | "platinum"
+  | "master"
+  | "grandmaster";
+
+type Group = {
+  number: number;
   games: Game[];
-  standings: Score[];
+  standings: Standing[];
 };
 
-{
-  master: [
-    {
-      division: 1,
-      games: [
-        {
-          name: "Game 1",
-          code: "BLABLA",
-          result: [
-            { player: "red", score: 225 },
-            { player: "blue", score: "RESIGNED" },
-            { player: "green", score: "TIMED OUT" },
-          ],
-          finished: "ISO-DATE",
-        },
-      ],
-      standing: [
-        { player: "red", points: 24.5 },
-        { player: "yellow", points: 37.2 },
-        { player: "blue", points: 11.5 },
-        { player: "green", points: 8.5 },
-      ],
-    },
-  ];
+type Game = {
+  name: string;
+  code: string;
+  date: string;
+  scores: { player: string; score: number | "RESIGNED" | "TIMED OUT" }[];
+};
+
+type Standing = {
+  player: string;
+  points: number;
+};
+
+const scoring: { [key: number]: { [key: number]: number } } = {
+  3: { 1: 5, 2: 2, 3: 0 },
+  4: { 1: 6, 2: 3, 3: 1, 4: 0 },
+};
+
+function processRaw(parsed: RawParsed[]) {
+  const result: { [key in League]?: { [key: number]: Group } } = {};
+
+  for (let item of parsed) {
+    const [_, le, gr, ga] = item.name.match(/\s(\w+)\s(\d+)\sgame\s(\d+)/)!;
+
+    const league = le.toLowerCase() as League;
+    const groupNumber = parseInt(gr, 10);
+    const gameNumber = parseInt(ga, 10);
+
+    result[league] = result[league] || {};
+
+    const group = (result[league]![groupNumber] = result[league]![
+      groupNumber
+    ] || {
+      number: groupNumber,
+      games: [],
+      standings: [],
+    });
+
+    const game = {
+      name: `Game ${gameNumber}`,
+      code: item.replayCode,
+      date: item.date,
+      scores: item.scores.map((score) => {
+        const player = score.length === 2 ? score[0] : score[1];
+        const points = score.length === 2 ? score[1] : parseInt(score[2], 10);
+        return {
+          player,
+          score:
+            points === "TIMED_OUT"
+              ? "TIMED OUT"
+              : (points as number | "RESIGNED" | "TIMED OUT"),
+        };
+      }),
+    };
+
+    game.scores.forEach((score, index) => {
+      let standing = group.standings.find((s) => s.player === score.player);
+      if (!standing) {
+        group.standings.push((standing = { player: score.player, points: 0 }));
+      }
+
+      if (score.score === "TIMED OUT") {
+        // No points for timing out
+        return;
+      }
+
+      // Find all players that have the same score, resignations included
+      // Take their ranking and then assign and divide points
+      const shared = game.scores
+        .map((sc, ix) => (sc.score === score.score ? ix + 1 : 0))
+        .filter((rank) => rank);
+
+      standing.points +=
+        shared.reduce(
+          (acc, rank) => acc + scoring[game.scores.length][rank],
+          0
+        ) / shared.length;
+    });
+
+    group.games.push(game);
+  }
+
+  return result;
 }
 
-function processRaw(parsed: RawParsed[]) {}
+const r = processRaw(extractRaw("test-data/international-season-14.html"));
 
-console.log(extractRaw("test-data/international-season-14.html").length);
+console.log(JSON.stringify(r, null, 2));
+
+// processRaw(extractRaw("test-data/intermezzo-season-11.html").slice(4, 5));
